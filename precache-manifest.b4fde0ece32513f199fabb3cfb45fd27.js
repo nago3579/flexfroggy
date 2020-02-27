a077017cc28532a35023b53a803abffa8248954d
nago3579 <nago3579@colorado.edu>
2020-02-27 14:44:58 -0700

    Updates

---
+++ precache-manifest.b4fde0ece32513f199fabb3cfb45fd27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a4d3356007f270528606c43f6f8369",
+    "url": "/flexfroggy/index.html"
+  },
+  {
+    "revision": "3e11d779fa9ed20e0d9c",
+    "url": "/flexfroggy/static/css/main.97994575.chunk.css"
+  },
+  {
+    "revision": "d2809339eb27891560b2",
+    "url": "/flexfroggy/static/js/2.22572849.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/flexfroggy/static/js/2.22572849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e11d779fa9ed20e0d9c",
+    "url": "/flexfroggy/static/js/main.02ef6a10.chunk.js"
+  },
+  {
+    "revision": "bc481fb6c12c78e1c99b",
+    "url": "/flexfroggy/static/js/runtime-main.c9f37e9e.js"
+  }
+]);
